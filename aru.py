e5dd067d8e8aebc17fd801c3900f1cde390bea5d
Denis <prohibiti@gmail.com>
2016-09-07 22:59:35 +0300

    get ad works

--- aru.py
+++ aru.py
@@ -4,57 +4,84 @@
 
 import os.path
 import urllib.request
+import urllib.parse
 import json
 
 
-def load_page(page):
-    '''load data from auto.ria.ua category: tractor, price: less than 40000uah'''
-    url = 'https://auto.ria.com/blocks_search_ajax/search/?category_id=4'
-    url += '&currency=3&countpage=100&bodystyle[2]=66&price_do=40000&page=' + str(page)
-    response = urllib.request.urlopen(url)
-    content = response.read()
-    content = content.decode('utf8')
+class Aru():
+    MAIN_URL = 'https://auto.ria.com/'
+    path_tolistofcars = './listofcars.json'
 
-    with open('./tmp_riaua', 'w', encoding='utf-8') as f:
-        f.write(content)
-    return
+    def __init__(self, category=4, price_min=0, price_max=0, currency=3,
+                 countpage=100):
+        self.category = category
+        self.price_max = price_max
+        self.price_min = price_min
+        self.currency = currency
+        self.countpage = countpage
+        return
+
+    def load_page(self, page=0):
+        '''load list of cars from auto.ria.ua by given parameters'''
+        params_dict = {
+            'price_do': self.price_max,
+            'price_ot': self.price_min,
+            'currency': self.currency,
+            'page': page,
+            'category_id': self.category,
+            'countpage': self.countpage,
+            'bodystyle[2]': 66}
+
+        params = urllib.parse.urlencode(params_dict)
+        url = self.MAIN_URL + 'blocks_search_ajax/search/?'
+        url += params
+        response = urllib.request.urlopen(url)
+        content = response.read()
+        content = content.decode('utf8')
+        with open('./listofcars.json', 'w', encoding='utf-8') as f:
+            f.write(content)
+        return
+
+    def get_cars_id(self):
+        with open(self.path_tolistofcars, 'r', encoding='utf-8') as f:
+            data = json.loads(f.read())
+        self.cars_ids = data['result']['search_result']['ids']
+
+    def load_car_info(self, car_id):
+        url = self.MAIN_URL + 'demo/bu/searchPage/v2/view/auto/'
+        url += car_id
+        # maybe delete it?
+        url += '/?lang_id=2'
+        response = urllib.request.urlopen(url)
+        content = response.read()
+        content = content.decode('utf8')
+        return content
 
 
-def get_cars_id():
-    with open('./tmp_riaua','r', encoding='utf-8') as f:
-        data = json.loads(f.read())
-    return data['result']['search_result']['ids']
-
-
-def load_car_info(car_id):
-    url = 'https://auto.ria.com/demo/bu/searchPage/v2/view/auto/' + car_id + '/?lang_id=2'
-    response = urllib.request.urlopen(url)
-    content = response.read()
-    content = content.decode('utf8')
-    return content
-
-    
 def main():
-    load_page(0)
-    cars_id = get_cars_id()
-    car = load_car_info(cars_id[1])
-    with open('./cartmp','w', encoding='utf-8') as f:
-        f.write(car)
-    with open('./cartmp','r', encoding='utf-8') as f:
-        car = json.loads(f.read())
+    tr = Aru(price_min=15000, price_max=45000)
+    #tr.load_page()
+    tr.get_cars_id()
+    # ITERATE LIST OF CARS
+    for i in range(1, 10):
+        car = tr.load_car_info(tr.cars_ids[i])
+        with open('./cartmp', 'w', encoding='utf-8') as f:
+            f.write(car)
+        with open('./cartmp', 'r', encoding='utf-8') as f:
+            car = json.loads(f.read())
 
-    fcar = {}
-    fcar['$'] = car['USD']
-    fcar['title'] = car['title']
-    fcar['Добавлено'] = car['addDate']
-    fcar['linkToView'] = car['linkToView']
-    fcar['Модель'] = car['modelName']
-    fcar['Марка'] = car['markName']
-    fcar['грн'] = car['UAH']
-    fcar['url'] = car['linkToView']
+        fcar = {}
+        fcar['$'] = car['USD']
+        fcar['title'] = car['title']
+        fcar['Добавлено'] = car['addDate']
+        fcar['Модель'] = car['modelName']
+        fcar['Марка'] = car['markName']
+        fcar['грн'] = car['UAH']
+        fcar['url'] = car['linkToView']
 
-    #for k,v in fcar.items():
-    #    print(k , v)
+        for k, v in fcar.items():
+            print(k, v)
+        print('+++++++++++++')
     #domain = 'https://auto.ria.com/'
     #response = urllib.request.urlopen(domain + fcar['url'])
     #content = response.read()
