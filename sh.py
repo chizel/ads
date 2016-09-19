7f6f266df2d63474070481087eb6aac0f65283cc
Denis <prohibiti@gmail.com>
2016-09-19 15:42:41 +0300

    works

--- sh.py
+++ sh.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import datetime
 import sqlite3
 from urllib.request import urlopen
 import urllib.parse
-import re
 import sqlite3
 from bs4 import BeautifulSoup
 
@@ -40,34 +40,64 @@ class Olx():
         self.url += 'q-' + urllib.parse.quote(query)
         self.url += '?' + params
 
-    def get_page(self, page_id=0):
-        #response = urlopen(self.url)
-        #content = response.read()
-        #content = content.decode('utf8')
-        #with open('./olxpage.html', 'w', encoding='utf-8') as f:
-        #    f.write(content)
-        #print('Page loaded')
-        #s = BeautifulSoup(response.read())
-        with open('./olxpage.html', 'r') as f:
-            s = BeautifulSoup(f.read())
-        ads = s.findAll('td', attrs={'class': 'offer'})
-        #ads = s.find_all('table', attrs={'summary': 'Объявление'})
-        # TODO what it's for
-        re_price = re.compile('''<strong>(.*)</strong>''')
+    def get_page(self, page_id=0, from_web=True):
+        if from_web:
+            response = urlopen(self.url)
+            content = response.read()
+            content = content.decode('utf8')
+            with open('./olxpage.html', 'w', encoding='utf-8') as f:
+                f.write(content)
+            print('Page loaded')
+            s = BeautifulSoup(response.read())
+        else:
+            with open('./olxpage.html', 'r') as f:
+                s = BeautifulSoup(f.read())
+
         ad_data = []
+        ads = s.findAll('td', attrs={'class': 'offer'})
+
+        # connecting to db
+        conn = sqlite3.connect(self.DB_NAME)
+        self.cursor = conn.cursor()
 
         for ad in ads:
+            # empty ad
             if len(str(ad)) < 100:
                 continue
+
+            # extracting ad link and title
+            tmp_tag = ad.find('a', attrs={'class': 'link'})
+            url = tmp_tag['href']
+
+            # position of the end of the link
+            pos = url.find('html') + 4
+
+            # TODO promoted link - remove it? mark it?
+            # is the link promoted
+            #if url.find('promoted') > 0:
+            #    print('promoted')
+
+            url = url[:pos]
+
+            # check if ad in the db
+            item_exists = self.cursor.execute(
+                'select count(*) from olxcars where url=?',
+                (url,)
+                )
+            if self.cursor.fetchone()[0]:
+                print('yep')
+                continue
+            print('nope')
+            exit()
+
+            #TODO remove any special symbols (.,'", etc.)
+            title = tmp_tag.strong.contents[0].lower()
+
             # extracting image link
             tmp_tag = ad.find('img')
             if tmp_tag:
                 img_src = tmp_tag['src']
-            # extracting ad link
-            # TODO promoted - remove it? mark it?
-            tmp_tag = ad.find('a', attrs={'class': 'link'})
-            url = tmp_tag['href']
-            title = tmp_tag.strong.contents[0]
+
             # extracting price
             tmp_tag = ad.find('p', attrs={'class': 'price'})
             tmp_price = re_price.search(str(tmp_tag.strong)).group(1)
@@ -75,33 +105,50 @@ class Olx():
             tmp_price = tmp_price.split()
             price = ''.join(tmp_price[:-1])
             price = int(price)
+
             # TODO do i need location now?
             # extracting location
             #tmp_tag = ad.find(
             #    'p', attrs={'class': 'color-9 lheight16 marginbott5'})
             #location = tmp_tag.span.contents[0]
             #location = location.strip()
+
             # extracting date
             # TODO convert date
             tmp_tag = ad.find(
                 'p',
                 attrs={'class': 'color-9 lheight16 marginbott5 x-normal'})
-            date = tmp_tag.contents[0]
-            date = date.strip()
-            ad_data.append([title, url, price, date])
+            ad_date = tmp_tag.contents[0]
+            ad_date = self.convert_date(ad_date)
+            ad_data.append([title, url, ad_date, price])
         self.save_to_db(ad_data)
-#            print(title, '||', price, '||', date, '||', )#location)
-#            print('==========================')
+        conn.commit()
+        conn.close()
+
+    def convert_date(self, ad_date):
+        ad_date = ad_date.strip()
+        ad_date = ad_date.lower()
+        if ad_date.find('сегодня') > -1:
+            return datetime.date.today()
+        if ad_date.find('вчера') > -1:
+            return datetime.date.today() - datetime.timedelta(1)
+        day, month_name = ad_date.split()
+
+        months = {
+            'янв.': '01', 'фев.': '02', 'март': '03', 'апр.': '04',
+            'май': '05', 'июнь': '06', 'июль': '07', 'авг.': '08',
+            'сент.': '09', 'окт.': '10', 'нояб.': '11', 'дек.': '12'}
+
+        result = str(datetime.date.today().year) + '-' + months[month_name]
+        result += '-' + day
+        return result
 
     def save_to_db(self, ad_data):
-        conn = sqlite3.connect(self.DB_NAME)
-        cursor = conn.cursor()
-        cursor.executemany(
+        #create table olxcars (title char, url char, added datetime, uah int);
+        self.cursor.executemany(
             #'INSERT INTO olxcars (title, url, uah, added) VALUES (?,?,?,?)',
             'INSERT INTO olxcars VALUES (?,?,?,?)',
             ad_data)
-        conn.commit()
-        conn.close()
 
 
 def main():
@@ -116,7 +163,8 @@ def main():
         #'currency': 'uah',
     }
     olx = Olx(query, **params)
-    olx.get_page()
+    olx.convert_date('9 сент.')
+    olx.get_page(from_web=False)
     return
 
 
